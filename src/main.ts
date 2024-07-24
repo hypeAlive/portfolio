b79844c546df4d9f18aba1c298c3eb0de26b2882
Alive <nicolasfr002@gmail.com>
2024-07-24 22:34:59 +0200

    feat(project): init i18n
    Took 1 hour 36 minutes
    
    Took 30 seconds

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
+import '@angular/localize/init';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
+import {enableProdMode} from "@angular/core";
 
-bootstrapApplication(AppComponent, appConfig)
+if (environment.production) {
+  enableProdMode();
+}
+
+
+bootstrapApplication(AppComponent, {
+  ...appConfig
+})
   .catch((err) => console.error(err));
