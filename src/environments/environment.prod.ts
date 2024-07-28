d5a29c68901fa6dbf356cd1035dd2df1b343ab0c
Alive <nicolasfr002@gmail.com>
2024-07-28 22:17:35 +0200

    feat(project): added logger
    Took 1 hour 31 minutes
    
    Took 10 seconds

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
+import {LogLevel} from "../app/shared/logger/log-level.enum";
+
 export const environment = {
   production: true,
   apiUrl: 'http://localhost:3000/api',
+  logLevel: LogLevel.ERROR
 };
