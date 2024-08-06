d51a6cb8cbdd93bc0aafc9a12dc283b050d3d8cd
Alive <nicolasfr002@gmail.com>
2024-08-06 12:33:18 +0200

    feat: added devicon via ng-icons :)
    Took 47 minutes

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import {CardComponent, PROGRAMMING_LANGUAGES} from "../shared/card/card.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CardComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
+    protected readonly PROGRAMMING_LANGUAGES = PROGRAMMING_LANGUAGES;
 }
--- src/app/page/shared/card/card.component.ts
+++ src/app/page/shared/card/card.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {NgIcon, provideIcons} from "@ng-icons/core";
+import {
+  diAngularOriginal,
+  diExpressOriginal,
+  diJavaOriginal,
+  diJavascriptOriginal,
+  diPythonOriginal, diTypescriptOriginal
+} from "@ng-icons/devicon/original";
+import {NgForOf} from "@angular/common";
+
+export enum PROGRAMMING_LANGUAGES {
+  ANGULAR = diAngularOriginal,
+  JAVA = diJavaOriginal,
+  JAVASCRIPT = diJavascriptOriginal,
+  TYPESCRIPT = diTypescriptOriginal,
+  PYTHON = diPythonOriginal,
+  EXPRESS = diExpressOriginal,
+}
 
 @Component({
-  selector: 'app-card',
+  selector: 'project-card',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIcon,
+    NgForOf
+  ],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  styleUrl: './card.component.scss',
+  viewProviders: [provideIcons({
+    diAngularOriginal,
+    diExpressOriginal,
+    diJavaOriginal,
+    diJavascriptOriginal,
+    diPythonOriginal, diTypescriptOriginal
+  })]
 })
 export class CardComponent {
+  @Input('languages') languages: PROGRAMMING_LANGUAGES[] = [];
+
+  protected getLangauges(): string[] {
+    return this.languages as any as string[];
+  }
 
 }
