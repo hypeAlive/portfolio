4eb27c64df7fc09c7dc2e58147fd78ab53342ddf
nicolas <nicolasfr.privat@gmail.com>
2024-09-26 16:06:58 +0200

    feat: added options to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,28 @@
 import {Routes} from '@angular/router';
 import errorRoutes from "./features/error/error.routes";
 import {ProjectResolver} from "./features/project/services/project.resolver";
+import {HeaderBackground} from "./core/services/header.service";
 
 export const routes: Routes = [
   {
     path: "",
-    loadComponent: () => import("./features/home/pages/home/home.component")
+    loadComponent: () => import("./features/home/pages/home/home.component"),
+    data: {
+      header: {
+        showMenu: true,
+        background: HeaderBackground.BLEND_IN_ON_SCROLL
+      }
+    }
   },
   {
     path: "project/:id",
     loadComponent: () => import("./features/project/pages/project/project.component"),
+    data: {
+      header: {
+        showMenu: false,
+        background: HeaderBackground.SHOW
+      }
+    },
     resolve: {
       project: ProjectResolver
     }
