cf281ad86ad1c4f22c90bc2b790b9d794bde2d5d
nicolas <nicolasfr.privat@gmail.com>
2024-09-26 14:40:15 +0200

    feat: implemented HeaderService

--- src/app/core/services/header.service.ts
+++ src/app/core/services/header.service.ts
@@ -1,9 +1,57 @@
-import { Injectable } from '@angular/core';
+import {Injectable, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, NavigationEnd, Router} from "@angular/router";
+import {filter, Subscription} from "rxjs";
+
+export type HeaderConfig = {
+  showMenu: boolean,
+  background: HeaderBackground
+}
+
+export enum HeaderBackground {
+  SHOW = 'show',
+  BLEND_IN_ON_SCROLL = 'blend-in-on-scroll',
+  TRANSPARENT = 'transparent',
+}
 
 @Injectable({
   providedIn: 'root'
 })
-export class HeaderService {
+export class HeaderService implements OnDestroy {
 
-  constructor() { }
+  private static readonly DEFAULT_CONFIG: HeaderConfig = {
+    showMenu: false,
+    background: HeaderBackground.SHOW
+  }
+
+  private currentConfig: HeaderConfig = HeaderService.DEFAULT_CONFIG;
+  private routeSubscription: Subscription | null = null;
+
+  constructor(private router: Router, private route: ActivatedRoute) {
+    this.routeSubscription = this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        const routeConfig = this.getCurrentRouteConfig();
+        this.currentConfig = routeConfig ? routeConfig : HeaderService.DEFAULT_CONFIG;
+      });
+  }
+
+  ngOnDestroy(): void {
+    if(!this.routeSubscription) return;
+
+    this.routeSubscription.unsubscribe();
+  }
+
+  private getCurrentRouteConfig(): HeaderConfig | null {
+    let currentRoute = this.route.root;
+    while (currentRoute.firstChild) {
+      currentRoute = currentRoute.firstChild;
+    }
+
+    return currentRoute.snapshot.data['header'] as HeaderConfig;
+  }
+
+  public getHeaderConfig(): HeaderConfig {
+    return this.currentConfig;
+  }
+
 }
