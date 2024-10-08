ff72e2034ef92183497eeb98b4b4537c021e76c4
Alive <nicolasfr002@gmail.com>
2024-10-08 11:43:46 +0200

    feat: footer setup for error page
    Took 7 minutes

--- src/app/features/error/error.routes.ts
+++ src/app/features/error/error.routes.ts
@@ -1,13 +1,25 @@
 import {Routes} from "@angular/router";
 import {ErrorStatusCode} from "./models/error-state";
-import {HeaderBackground} from "../../core/services/header.service"; // Achte darauf, dass der Pfad korrekt ist
+import {HeaderBackground} from "../../core/services/header.service";
+import {FooterBackground} from "../../core/services/footer.service";
 
 const routes: Routes = [
   ...Object.values(ErrorStatusCode).map(statusCode => ({
     path: statusCode.toString(),
     loadComponent: () => import('./pages/error/error.component'),
     title: statusCode.toString(),
-    data: {statusCode, header: {showMenu: false, background: HeaderBackground.SHOW}}
+    data: {
+      statusCode,
+      header: {
+        showMenu: false,
+        background: HeaderBackground.SHOW
+      },
+      footer: {
+        small: true,
+        background: FooterBackground.HALF_OPACITY,
+        fixed: true
+      },
+    }
   })),
   {
     path: "**",
