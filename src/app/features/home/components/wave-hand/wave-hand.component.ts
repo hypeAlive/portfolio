675a8a30999df2764f5f8c273eb312a8687cb992
NicolasMacBook <nicolas@macBook.com>
2024-10-16 13:45:55 +0200

    feat: added wave hand component for a waving hand
    Took 20 seconds

---
+++ src/app/features/home/components/wave-hand/wave-hand.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home-wave-hand',
+  standalone: true,
+  imports: [],
+  templateUrl: './wave-hand.component.html',
+  styleUrl: './wave-hand.component.scss'
+})
+export class WaveHandComponent {
+
+}
