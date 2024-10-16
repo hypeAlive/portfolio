5a3c4d78a69bf95cc7fd8d8fc1a4781d44b019b4
NicolasMacBook <nicolas@macBook.com>
2024-10-16 14:49:14 +0200

    feat: added ability to fade in directive to set up delay
    Took 15 minutes

--- src/app/shared/directives/fade-in.directive.ts
+++ src/app/shared/directives/fade-in.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, OnDestroy, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, Input, OnDestroy, OnInit, Renderer2} from '@angular/core';
 import {NGXLogger} from "ngx-logger";
 
 @Directive({
@@ -7,12 +7,16 @@ import {NGXLogger} from "ngx-logger";
 })
 export class FadeInDirective implements OnInit, OnDestroy {
 
+  @Input() delay: string = '0s';
+
   private static observer: IntersectionObserver = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
+        (entry.target as HTMLElement).style.transitionDelay = entry.target.getAttribute('fade-in-delay') || '0s';
         entry.target.classList.add('fade-in');
       } else {
         entry.target.classList.remove('fade-in');
+        (entry.target as HTMLElement).style.transitionDelay = '0s';
       }
     });
   });
@@ -25,6 +29,7 @@ export class FadeInDirective implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.renderer.addClass(this.element.nativeElement, 'fade');
+    this.renderer.setAttribute(this.element.nativeElement, 'fade-in-delay', `${this.delay}`);
     this.observer.observe(this.element.nativeElement);
   }
 
