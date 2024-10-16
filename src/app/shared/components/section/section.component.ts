614944038fce033c0c4ffdfb96eb79da8d9fa067
NicolasMacBook <nicolas@macBook.com>
2024-10-16 13:45:34 +0200

    feat: added section component with settings for waves and background
    Took 39 minutes

--- src/app/shared/components/section/section.component.ts
+++ src/app/shared/components/section/section.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {NgClass, NgIf} from "@angular/common";
+
+export enum SectionWave {
+  TOP = 'top',
+  BOTTOM = 'bottom',
+  BOTH = 'both',
+  NONE = 'none'
+}
 
 @Component({
   selector: 'app-section',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    NgClass
+  ],
   templateUrl: './section.component.html',
   styleUrl: './section.component.scss'
 })
 export class SectionComponent {
+
+  @Input('withWave') withWave: SectionWave = SectionWave.NONE;
+  @Input('withGradient') withGradient = false;
+
+  protected isWaveTop(): boolean {
+    return this.withWave === SectionWave.TOP || this.withWave === SectionWave.BOTH;
+  }
+
+  protected isWaveBottom(): boolean {
+    return this.withWave === SectionWave.BOTTOM || this.withWave === SectionWave.BOTH;
+  }
 
 }
