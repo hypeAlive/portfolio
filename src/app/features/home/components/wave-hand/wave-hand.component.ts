ad4c4cd2d5b475847a709d05d0ff0f0e1db6802f
NicolasMacBook <nicolas@macBook.com>
2024-10-16 13:46:58 +0200

    refactor: removed html of wave component
    Took 1 minute

--- src/app/features/home/components/wave-hand/wave-hand.component.ts
+++ src/app/features/home/components/wave-hand/wave-hand.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'home-wave-hand',
   standalone: true,
   imports: [],
-  templateUrl: './wave-hand.component.html',
+  template: '<span class="wave">👋</span>',
   styleUrl: './wave-hand.component.scss'
 })
 export class WaveHandComponent {
