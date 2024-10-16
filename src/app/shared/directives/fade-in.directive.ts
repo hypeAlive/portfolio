b2dbac0d9c76aa4d3cc7d625436535b1af63df15
Alive <nicolasfr002@gmail.com>
2024-10-16 21:14:52 +0200

    feat: added ability to fade in directive to fade in once
    Took 11 minutes

--- src/app/shared/directives/fade-in.directive.ts
+++ src/app/shared/directives/fade-in.directive.ts
@@ -8,13 +8,22 @@ import {NGXLogger} from "ngx-logger";
 export class FadeInDirective implements OnInit, OnDestroy {
 
   @Input() delay: string = '0s';
+  /**
+   * If true, the element will fade in again when it comes back into view
+   */
+  @Input() reFade: boolean = true;
 
   private static observer: IntersectionObserver = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
         (entry.target as HTMLElement).style.transitionDelay = entry.target.getAttribute('fade-in-delay') || '0s';
         entry.target.classList.add('fade-in');
+        if (entry.target.getAttribute('fade-in-re-fade') === 'false') {
+          FadeInDirective.observer.unobserve(entry.target);
+        }
       } else {
+        if (entry.target.getAttribute('fade-in-re-fade') === 'false') return;
+
         entry.target.classList.remove('fade-in');
         (entry.target as HTMLElement).style.transitionDelay = '0s';
       }
@@ -30,6 +39,7 @@ export class FadeInDirective implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.renderer.addClass(this.element.nativeElement, 'fade');
     this.renderer.setAttribute(this.element.nativeElement, 'fade-in-delay', `${this.delay}`);
+    this.renderer.setAttribute(this.element.nativeElement, 'fade-in-re-fade', `${this.reFade}`);
     this.observer.observe(this.element.nativeElement);
   }
 
