03ad7ee27844a382558232ae245d11b476f129e7
Alive <nicolasfr002@gmail.com>
2024-10-17 00:28:45 +0200

    feat: added point gradient bg effect
    Took 48 minutes

---
+++ src/app/shared/components/point-gradient/point-gradient.component.ts
@@ -0,0 +1,30 @@
+import {Component, Input} from '@angular/core';
+import {NgClass} from "@angular/common";
+
+export enum PointGradientType {
+  PRIMARY = 'primary',
+  PRIMARY_CONTENT = 'primary-content',
+  SECONDARY = 'secondary',
+  SECONDARY_CONTENT = 'secondary-content',
+  ACCENT = 'accent',
+  ACCENT_CONTENT = 'accent-content',
+}
+
+@Component({
+  selector: 'bg-point-gradient',
+  standalone: true,
+  imports: [
+    NgClass
+  ],
+  templateUrl: './point-gradient.component.html',
+  styleUrl: './point-gradient.component.scss'
+})
+export class PointGradientComponent {
+
+  @Input() type: PointGradientType = PointGradientType.SECONDARY;
+
+  protected getGradientClass(): string {
+    return `point-gradient ${this.type}`;
+  }
+
+}
